f53997450e640cfb98ed19ee5fd8fb27ca69cb76
Albert Garcia Llorca <albertgarcia20@gmail.com>
2022-11-01 00:22:23 +0000

    - showNotifications with dynamic messaging

--- src/main/webapp/js/tools.js
+++ src/main/webapp/js/tools.js
@@ -7,12 +7,31 @@
  * @func showNotification
  * Mostra notificacions als usuaris en funció de respostes obtingudes del servidor.
  * @param resposta El contingut de la resposta generada pel servidor
- * @param missatges Objecte que encapsula missatges en funció dels codis retornats pel servidor -> 2XX: OK; 201: Recurs creat; 4XX: Error en el client, recurs no trobat; 5XX: Error en el servidor, petició no completada
+ * @param missatges Objecte que encapsula missatges en funció dels codis retornats pel servidor -> 2XX: OK; 201: Recurs creat; 4XX: Error en el client, recurs no trobat; 5XX: Error en el servidor, petició no completada, etc.
  *  
  */
 export default function showNotification(resposta, missatges) {
     //TODO processar la resposta i generar notificacions
-    let cosNotificacio = resposta;
+    let catalegMissatges = {
+        "500": "Error intern del servidor. Operació no completada",
+        "511": "Autenticació de xarxa és necessària per accedir",
+        "501": "Mètode no implementat",
+        "502": "Resposta del servidor invàlida",
+        "503": "Servidor no disponible",
+        "400": "Petició errònia o mal formada, no es pot continuar",
+        "401": "Cal autenticar-se per poder obtenir una resposta del servidor",
+        "403": "Accés prohibit al recurs, permisos insuficients",
+        "404": "No es pot trobar el recurs sol·licitat",
+        "405": "Mètode no permès pel servidor",
+    };
+
+    //Afegim un cas genèric per a codis no previstos
+    if(catalegMissatges[resposta.status] == undefined){
+        catalegMissatges[resposta.status] = "El servidor ha respost amb "+resposta.status+". Contacta amb l'administrador del web per a més informació";
+    }
+
+    $.extend(catalegMissatges, missatges);
+    let cosNotificacio = catalegMissatges[resposta.status];
     //Crear notificació
     $('<div class="toast-container position-fixed bottom-0 end-0 p-3">     <div id="notificacio" class="toast" role="alert" aria-live="assertive" aria-atomic="true">     <div class="toast-header">        <i class="fa-solid fa-circle-info p-1"></i>        <strong class="me-auto">Notificació</strong>        <small></small>        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Tanca"></button>      </div>      <div class="toast-body">       '+ cosNotificacio +'      </div>    </div>  </div>').appendTo("body");
     //Inicialitzar la notificació al JS de Bootstrap
