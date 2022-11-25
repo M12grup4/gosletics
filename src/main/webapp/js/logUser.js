d1b50612b1fae54ac677f37138a40ee732b384f1
Albert Garcia Llorca <albertgarcia20@gmail.com>
2022-11-25 10:59:44 +0000

    - Logging works

--- src/main/webapp/js/logUser.js
+++ src/main/webapp/js/logUser.js
@@ -10,13 +10,15 @@ let mail;
 let pass;
 
 /**
- * API calls
+ * API calls and destinations
  */
 const POST_LOGIN = WEBROOT + "/login/valida";
 //const GET_LOGOUT = WEBROOT + "";
+const RED_ADMIN = "./dogManagement.html";
+const RED_USER = "./bookingManagement.html";
 
 /**
- * Quan el DOM està carregat, assigna els camps de formulari a les variables de camp
+ * Quan el DOM està carregat, assigna els camps de formulari a les variables de camp.
  */
 $().ready(() => {
     mail = $('#inputName');
@@ -25,8 +27,25 @@ $().ready(() => {
 });
 
 /**
+ * @function redirect
+ * Aquesta funció redirigeix l'usuari en funció dels seus privilegis.
+ * @param {String} isAdmin String de control true o false
+ */
+function redirect(isAdmin) {
+    if (isAdmin == "true") {
+        window.location.replace(RED_ADMIN);
+    } else if (isAdmin == "false") {
+        window.location.replace(RED_USER);
+    } else {
+        //undefined, do nothing
+    }
+}
+
+/**
  * @function sendLogin
  * Envia dades introduïdes per l'usuari al servidor per autenticar-se a l'aplicació.
+ * Desa dades mínimes retornades pel servidor al local storage per poder referir-se
+ * a l'usuari en altres parts de l'aplicació.
  * 
  */
 function sendLogin() {
@@ -48,11 +67,22 @@ function sendLogin() {
         success: (result, status, jqxhr) => {
             mail.val("");
             pass.val("");
-            let r = JSON.parse(result.substring(5));
-            localStorage.setItem(id, r.id);
-            localStorage.setItem(mail, r.mail);
-            localStorage.setItem(isAdmin, r.isAdmin);
-            showNotification(jqxhr, { "200": "Benvingut, " + userData.mail + "!" });
+            let r = result.split('#');
+            localStorage.setItem('id', r[0]);
+            localStorage.setItem('mail', r[1]);
+            localStorage.setItem('isAdmin', r[2]);
+
+            if (r[0] != -1) {//Si el login és correcte
+                showNotification(jqxhr, { "200": "Benvingut, " + localStorage.getItem('mail') + "!" });
+                redirect(r[2]);
+
+            } else {//Si el login no és correcte
+                localStorage.removeItem('id');
+                localStorage.removeItem('mail');
+                localStorage.removeItem('isAdmin');
+                showNotification(jqxhr, { "200": "Credencials incorrectes, revisa les dades." });
+            }
+
         },
         error: (error) => {
             console.log(error);
@@ -80,5 +110,9 @@ function logout() {
             showNotification(error, { "0": "Error al desconnectar-se. Prova-ho novament." });
         },
     });
+    //Neteja local storage
+    localStorage.removeItem('id');
+    localStorage.removeItem('mail');
+    localStorage.removeItem('isAdmin');
 }
 
