33839ec0c5a7142e1531d005815c5cf8db40d57b
CONXI <CONXI@DESKTOP-1MB8FG0>
2022-11-27 12:47:32 +0100

    Goslètic 27/11/22 - s'afegeix ruta de logout Logincontroller.java: /login/logout LoginDAO.java: surtLogin()

--- src/main/java/cat/xtec/ioc/controller/LoginController.java
+++ src/main/java/cat/xtec/ioc/controller/LoginController.java
@@ -2,12 +2,13 @@
  * @author juanrodriguezguardeno & Conxi Gàlvez Baixench
  * @version TEA3 M12grup4
  * @file LoginController.java
+ * Goslètic 27/11/22 - s'afegeix ruta de logout 
+ *      Logincontroller.java: /login/logout
+ *      LoginDAO.java: surtLogin()
  * @descripcio Fitxer que recull totes les gestions de login
- * 
-	Valida un usuari des del formulari de Login
-	@RequestMapping(value = "/login/valida", method =  RequestMethod.POST)
+ *  - Valida un usuari des del formulari de Login (POST) a /login/valida -> validaLogin(login)
+ *  - Logout d'usuari (POST) a /logint/logout -> surtLogin(login)
  */
-
 package cat.xtec.ioc.controller;
 
 import cat.xtec.ioc.domain.Login;
@@ -22,32 +23,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class LoginController {
  
-@Autowired
-private LoginDAO loginDAO;
+    @Autowired
+    private LoginDAO loginDAO;
 
-public  LoginController() {
-    
-}
+    public  LoginController() {
+    }
     
     public LoginController(LoginDAO loginDAO) {
-    
-    this.loginDAO = loginDAO;
-}
+        this.loginDAO = loginDAO;
+    }
 
-    
+    // mapeig del Login d'usuari
     @RequestMapping(value = "/login/valida", method =  RequestMethod.POST)
     public ResponseEntity validaLogin(@RequestBody Login login) throws SQLException, IOException{
-       
         String resultado=loginDAO.validaLogin(login);
         // passem objecte Login a format JSON
         //String resultadoStr = resultado.toString();
+        return new ResponseEntity(resultado, HttpStatus.OK);     
+    }
+    
+    // mapeig del Logout d'usuari
+    @RequestMapping(value = "/login/logout", method =  RequestMethod.POST)
+    public ResponseEntity logoutUser(@RequestBody Login login) throws SQLException, IOException{
+        String resultado=loginDAO.surtLogin(login);
         return new ResponseEntity(resultado, HttpStatus.OK);
-      
-      
     }
 }
